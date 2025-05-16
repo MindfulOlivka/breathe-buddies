32efa456255aa0650c555a66ada827cb855d5f17
MindfulOlivka <o.campbell@ufl.edu>
2025-05-15 20:45:02 -0400

    adding condition function

---
+++ app.js
@@ -0,0 +1,17 @@
+let selectCondition = '';
+let currentAudio = null;
+
+function selectCondition(condition) {
+const animalDisplay = document.getElementById('animal-display');
+const animalGifs = {
+    Anxious:,
+Overwhelemed:,
+Sad:,
+Fatigued,
+}
+
+    selectCondition = condition;
+    document.getElementById('generate-section').style.display = 'block';
+    
+}
+
